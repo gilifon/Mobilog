dd66d205ce0c2f24664c57f824f080e2159895d8
Gil (4Z1KD) <gil4z1kd@gmail.com>
2017-10-14 08:38:57 +0300

    MobileApp

---
+++ MobilogApp/app/src/main/java/org/a4z1kd/mobilog/MainActivity.java
@@ -0,0 +1,13 @@
+package org.a4z1kd.mobilog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
